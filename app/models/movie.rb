3afe81e40576c5c6edd2b9540af69ceead15de30
sebgomez2023 <sebgomez@uchicago.edu>
2023-03-02 07:41:25 +0000

    Added alerts and user and notice

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,4 +13,11 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+  belongs_to :director
+  
+  has_many :characters
+
+  has_many :cast, :through => :characters, :source => :actor
+
+  validates(:title, {:presence => true})
 end
